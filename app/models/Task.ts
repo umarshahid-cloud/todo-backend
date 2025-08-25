4e21c0066b214dbfa64edce4aec821276bbd6b0f
umarshahid-cloud <umar.shahid@mergestack.com>
2025-08-25 18:42:12 +0500

    added validation in schema

--- app/models/Task.ts
+++ app/models/Task.ts
@@ -3,7 +3,12 @@ import { ITask } from "@app/types/task";
 
 const TaskSchema = new Schema<ITask>(
   {
-    text: { type: String, required: true, trim: true },
+    text: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: [4, "Task must be at least 4 characters long"],
+    },
     isComplete: { type: Boolean, default: false },
     createdAt: { type: Date, default: Date.now },
   },
